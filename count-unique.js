837426471fc276c257a1bc51949be1fd64a330a2
vijayramkissoon <vramkissoon@proton.me>
2024-08-12 12:38:08 -0500

    Added debugging line to count-unique.js

--- count-unique.js
+++ count-unique.js
@@ -2,6 +2,9 @@ function handleInput() {
     // Get the value from the input field
     const inputText = document.getElementById('userInput').value;
 
+    //Debug
+    console.log('Input Text: ${inputText}');
+
     //Display the input value in the paragraph
     document.getElementById('displayText').innerText = 'You entered: ${inputText}';
 }
