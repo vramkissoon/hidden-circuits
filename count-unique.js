ce6638cc63a914ec6ef753fbd4c7e4251f3d764c
vijayramkissoon <vramkissoon@proton.me>
2024-08-12 12:02:06 -0500

    Created count-unique.js to handle input from nlp.html

---
+++ count-unique.js
@@ -0,0 +1,7 @@
+function handleInput() {
+    // Get the value from the input field
+    const inputText = document, getElementById('userInput').value;
+
+    //Display the input value in the paragraph
+    document.getElementById('displayText').innerText = 'You entered: ${inputText}';
+}
