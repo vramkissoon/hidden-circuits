2717a66fbdf04d4e73bac5fa42af410bdb03144f
vijayramkissoon <vramkissoon@proton.me>
2024-08-12 12:28:30 -0500

    Fix error

--- count-unique.js
+++ count-unique.js
@@ -1,6 +1,6 @@
 function handleInput() {
     // Get the value from the input field
-    const inputText = document, getElementById('userInput').value;
+    const inputText = document.getElementById('userInput').value;
 
     //Display the input value in the paragraph
     document.getElementById('displayText').innerText = 'You entered: ${inputText}';
